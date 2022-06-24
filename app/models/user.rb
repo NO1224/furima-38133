6d9acd99329bf48bc7caf43dd92c919cc6f9fac7
NO1224 <kiyurino16@gmail.com>
2022-06-24 22:41:13 +0900

    エラーハンドリング実装(重複エラーメッセージ対応済)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  VALID_NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX,
+                                 message: 'include both letters and numbers'},
+                                 allow_blank: true        
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { allow_blank: true,
+                                    with: VALID_NAME_REGEX, 
+                                    message: 'full-width kanji, hiragana and katakana characters'}
+    validates :first_name, format: { allow_blank: true,
+                                     with: VALID_NAME_REGEX,
+                                     message: 'full-width kanji, hiragana and katakana characters'}
+    validates :last_name_kana, format: { allow_blank: true,
+                                         with: VALID_NAMEKANA_REGEX,
+                                         message: 'full-width katakana characters'}
+    validates :first_name_kana, format: { allow_blank: true, 
+                                          with: VALID_NAMEKANA_REGEX, 
+                                          message: 'full-width katakana characters'}
+    validates :birthday
+  end
 end
