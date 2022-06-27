ed2e76a894c6d5808e171ab6f1ef222b7f8066b9
NO1224 <kiyurino16@gmail.com>
2022-06-28 00:01:23 +0900

    商品情報入力実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,28 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,
+                                 :name,
+                                 :explanation,
+                                 :category_id,
+                                 :condition_id,
+                                 :delivery_pay_id,
+                                 :delivery_source_id,
+                                 :delivery_time_id,
+                                 :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,26 +1,41 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
+  #Tableアソシエーション
+  belongs_to :user
+  #ActiveHashアソシエーション
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_pay
   belongs_to :delivery_source
   belongs_to :delivery_time
+  #ActiveStorageアソシエーション
+  has_one_attached :image
+
+  validate :image_content_type, if: :was_attached?
+
+  def image_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, "format or file extension is not valid") unless image.content_type.in?(extension)
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :explanation
-    validates :category_id,        numericality: { other_than: 1,
-                                                   message: "can't be blank" }
-    validates :condition_id,       numericality: { other_than: 1,
-                                                   message: "can't be blank" }
-    validates :delivery_pay_id,    numericality: { other_than: 1,
-                                                   message: "can't be blank" }
-    validates :delivery_source_id, numericality: { other_than: 1,
-                                                   message: "can't be blank" }
-    validates :delivery_time_id,   numericality: { other_than: 1,
-                                                   message: "can't be blank" }
-    validates :price
+    validates :price,      numericality: { allow_blank: true,
+                                           only_integer: true, 
+                                           greater_than: 300,
+                                           less_than: 9999999}
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_pay_id
+    validates :delivery_source_id
+    validates :delivery_time_id
   end
 end
