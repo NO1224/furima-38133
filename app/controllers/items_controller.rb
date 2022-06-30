33f1de8d16cce7d457ab51a41da45afb40e906fe
NO1224 <kiyurino16@gmail.com>
2022-06-30 15:13:14 +0900

    商品情報編集　基本MVC実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -18,7 +19,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -32,5 +38,9 @@ class ItemsController < ApplicationController
                                  :delivery_source_id,
                                  :delivery_time_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
