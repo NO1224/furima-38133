36909a882c5f2068f7de4f14ba9617d9d1e5351d
NO1224 <kiyurino16@gmail.com>
2022-07-01 16:23:34 +0900

    都道府県コラム名修正　その５

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  #Tableアソシエーション
   belongs_to :order
+  #ActiveHashアソシエーション
+  belongs_to :prefecture
 end
